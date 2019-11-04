c957f8b2ac6dc13e7b949132c65b89d32a91c0eb
Juho Kesälä <elukka.elukka@gmail.com>
2019-11-04 14:32:53 +0200

    Exercise 1.2 step 2

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -13,9 +13,9 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content part={part1} exercises={exercises1} />
-            <Content part={part2} exercises={exercises2} />
-            <Content part={part3} exercises={exercises3} />
+            <Content part1={part1} exercises1={exercises1}
+                part2={part2} exercises2={exercises2}
+                part3={part3} exercises3={exercises3} />
             <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
         </div>
     )
@@ -31,11 +31,11 @@ const Header = (props) => {
 
 const Content = (props) => {
     return (
-        <>
-            <p>
-                {props.part} {props.exercises}
-            </p>
-        </>
+        <div>
+            <Part part={props.part1} exercises={props.exercises1} />
+            <Part part={props.part2} exercises={props.exercises2} />
+            <Part part={props.part3} exercises={props.exercises3} />
+        </div>
     )
 }
 
@@ -44,6 +44,14 @@ const Total = (props) => {
         <>
             <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
         </>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <div>
+            {props.part} {props.exercises}
+        </div>
     )
 }
 
