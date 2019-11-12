814552d3da25a3680db9d84681c95409efaadf5d
Juho Kesälä <elukka.elukka@gmail.com>
2019-11-12 19:25:54 +0200

    Exercise 1.13

--- part1/anekdootit/src/index.js
+++ part1/anekdootit/src/index.js
@@ -1,19 +1,32 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
+// Ought to split App into multiple components
 const App = (props) => {
-    let randomIndex = randomAnecdoteIndex(props.anecdotes.length)
-    const [selected, setSelected] = useState(randomIndex)
+    const [selected, setSelected] = useState(randomIndex(props.anecdotes.length))
+    // Ideally should create an array of zeros based on the size of the anecdotes array, this assumes it stays the same.
+    // Would be an easy change if it was needed.
+    const [votes, setVotes] = useState( [ 0, 0, 0, 0, 0, 0 ] )
 
     return (
         <div>
-            {props.anecdotes[selected]}
+            <div>
+                {anecdotes[selected]}
+            </div>
+            <div>
+                {'has ' + votes[selected] + ' votes'}
+            </div>
+            <div>
+                <button onClick={() => upvoteAnecdote(selected, votes, setVotes)}>{'Vote'}</button>
+                <button onClick={() => setAnecdote(props.anecdotes, setSelected)}>{'Next anecdote'}</button>
+            </div>
         </div>
     )
 }
 
 const anecdotes = [
-    'If it hurts, do it more often',
+    'If it hurts, do it more often' ,
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
@@ -21,8 +34,18 @@ const anecdotes = [
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-const randomAnecdoteIndex = (anecdotesSize) => {
-    return Math.floor(Math.random() * anecdotesSize)
+const randomIndex = (anecdotesLength) => {
+    return Math.floor(Math.random() * anecdotesLength)
+}
+
+const setAnecdote = (anecdotes, setSelected) => {
+    setSelected(randomIndex(anecdotes.length))
+}
+
+const upvoteAnecdote = (selected, votes, setVotes) => {
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
 }
 
 ReactDOM.render(
