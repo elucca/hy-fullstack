e3a69aca2c7d0b5d9423840422decb5567b9a524
Juho Kesälä <elukka.elukka@gmail.com>
2019-11-17 18:42:41 +0200

    Exercise 2.1

---
+++ part2/kurssitiedot/src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({ parts }) => {
+    const rows = () => parts.map(part =>
+        <Part
+            key={part.id}
+            part={part}
+        />
+    )
+
+    return (
+        <div>
+            <ul>
+                {rows()}
+            </ul>
+        </div>
+    )
+}
+
+export default Content
--- part2/kurssitiedot/src/components/Course.js
+++ part2/kurssitiedot/src/components/Course.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Content from './Content'
+import Header from './Header'
 
 const Course = ({ course }) => {
     return (
-        <></>
+        <div>
+            <Header course={course.name} />
+            <Content name={course.name} parts={course.parts} />
+        </div>
     )
 }
 
