721dbcc4ad88f9a5023dcf73ee0db0a03b2f9d56
Juho Kesälä <elukka.elukka@gmail.com>
2020-11-23 19:02:13 +0200

    Exercise 4.4 & 4.5

--- part4/blogilista/src/utils/list_helper.js
+++ part4/blogilista/src/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
+  blogs
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, blog) => { return acc + blog.likes}, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const mostLikes = Math.max(...blogs.map(blog => blog.likes))
+  return blogs.find(blog => blog.likes === mostLikes)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
