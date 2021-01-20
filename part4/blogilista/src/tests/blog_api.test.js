a4906b4ca4676d72eaa3f40e7522d746356bb948
elucca <elucca@users.noreply.github.com>
2021-01-20 04:53:43 +0200

    Exercise 4.9

--- part4/blogilista/src/tests/blog_api.test.js
+++ part4/blogilista/src/tests/blog_api.test.js
@@ -23,6 +23,11 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('blogs have field named id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach(blog => expect(blog.id).toBeDefined())
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
