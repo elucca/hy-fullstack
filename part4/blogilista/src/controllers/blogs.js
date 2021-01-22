11ee15b8501f952c9af3171bf661518bb18c6095
elucca <elucca@users.noreply.github.com>
2021-01-22 18:30:03 +0200

    Begin exercise 4.17

--- part4/blogilista/src/controllers/blogs.js
+++ part4/blogilista/src/controllers/blogs.js
@@ -1,17 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const users = await User.find({})
+  const user = users[0]
 
-  if (blog.likes === undefined) {
-    blog.likes = 0
-  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes,
+    user: user === undefined ? undefined : user.id
+  })
 
   if (blog.title === undefined || blog.url === undefined) {
     response.status(400).end()
