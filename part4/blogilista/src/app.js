90eb965c706d4934e361a06ec7c468f86aa38707
elucca <elucca@users.noreply.github.com>
2021-01-22 19:28:18 +0200

    Exercise 4.18

--- part4/blogilista/src/app.js
+++ part4/blogilista/src/app.js
@@ -2,10 +2,14 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,5 +27,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
