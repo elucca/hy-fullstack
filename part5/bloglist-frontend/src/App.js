4b23bfb49dc6bdfa3c48dc5cc37cde67490654ef
elucca <elucca@users.noreply.github.com>
2021-01-24 18:55:00 +0200

    Add user's name to blogs page

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -57,6 +57,7 @@ const App = () => {
     return (
       <div>
         <h2>Blogs</h2>
+        <p>Logged in as {user.name}</p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
@@ -71,7 +72,5 @@ const App = () => {
     </div>
   )
 }
-
-
 
 export default App
