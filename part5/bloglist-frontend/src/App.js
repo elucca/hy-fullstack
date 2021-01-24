89d512d416f2d9333dfb08ced4496b5ac2454293
elucca <elucca@users.noreply.github.com>
2021-01-24 16:41:03 +0200

    Exercise 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,77 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Invalid username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 3000)
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>Username &nbsp;
+        <input type="text" value={username} name="Username"
+              onChange={({ target }) => setUsername(target.value)} />
+          </div>
+
+          <div>Password &nbsp;
+        <input type="text" value={password} name="Password"
+              onChange={({ target }) => setPassword(target.value)} />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }
+
+  const blogList = () => {
+    return (
+      <div>
+        <h2>Blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null && loginForm()}
+      {user !== null && blogList()}
     </div>
   )
 }
+
+
 
 export default App
