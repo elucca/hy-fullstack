4333faceb72cd625920f7c8bc41e367a425b4dce
elucca <elucca@users.noreply.github.com>
2021-01-25 08:52:30 +0200

    Exercise 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,76 +1,103 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
-import loginService from './services/login'
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBloglistUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     try {
-      const user = await loginService.login({ username, password })
-      setUser(user)
-      setUsername('')
-      setPassword('')
+      const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem("loggedBloglistUser", JSON.stringify(user));
+
+      setUser(user);
+      setUsername("");
+      setPassword("");
     } catch (exception) {
-      setErrorMessage('Invalid username or password')
+      setErrorMessage("Invalid username or password");
       setTimeout(() => {
-        setErrorMessage(null)
-      }, 3000)
+        setErrorMessage(null);
+      }, 3000);
     }
-  }
+  };
+
+  const handleLogout = async () => {
+    window.localStorage.removeItem("loggedBloglistUser");
+    setUser(null);
+  };
 
   const loginForm = () => {
     return (
       <div>
         <h2>Login</h2>
         <form onSubmit={handleLogin}>
-          <div>Username &nbsp;
-        <input type="text" value={username} name="Username"
-              onChange={({ target }) => setUsername(target.value)} />
+          <div>
+            Username &nbsp;
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
           </div>
 
-          <div>Password &nbsp;
-        <input type="text" value={password} name="Password"
-              onChange={({ target }) => setPassword(target.value)} />
+          <div>
+            Password &nbsp;
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
           </div>
 
           <button type="submit">Login</button>
         </form>
       </div>
-    )
-  }
+    );
+  };
 
   const blogList = () => {
     return (
       <div>
         <h2>Blogs</h2>
-        <p>Logged in as {user.name}</p>
-        {blogs.map(blog =>
+        <p>Logged in as {user.name} &nbsp;</p>
+
+        <button onClick={() => handleLogout()}>Logout</button>
+
+        {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
-        )}
+        ))}
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div>
       {user === null && loginForm()}
       {user !== null && blogList()}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
