c54ed0f6076ca2fce1d08b1408d0eff4af9bfd8c
elucca <elucca@users.noreply.github.com>
2021-02-28 17:10:01 +0200

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetailed, setShowDetailed] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const toggleShowDetailed = () => {
+    setShowDetailed(!showDetailed)
+  }
+
+  if (showDetailed) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} by {blog.author} &nbsp;
+        <button onClick={toggleShowDetailed}>Hide</button>
+        <br></br>
+        {blog.url} <br></br>
+        Likes: {blog.likes} &nbsp;
+        <button>Like</button>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} by {blog.author} &nbsp;
+        <button onClick={toggleShowDetailed}>Show</button>
+      </div>
+    )
+  }
+}
 
 export default Blog
