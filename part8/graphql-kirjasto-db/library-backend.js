6d4ca5e34ab7d68d03869db019c9d94f8c6ecfb3
Juho Kesälä <elukka.elukka@gmail.com>
2024-12-25 05:01:46 +0200

    Exercise 8.14

--- part8/graphql-kirjasto-db/library-backend.js
+++ part8/graphql-kirjasto-db/library-backend.js
@@ -13,11 +13,12 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 console.log('connecting to', MONGODB_URI)
 
-mongoose.connect(MONGODB_URI)
+mongoose
+  .connect(MONGODB_URI)
   .then(() => {
     console.log('connected to MongoDB')
   })
-  .catch((error) => {
+  .catch(error => {
     console.log('error connection to MongoDB:', error.message)
   })
 
@@ -62,29 +63,75 @@ const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
-    allBooks: async () => Book.find({}),
-    allAuthors: async () => Author.find({})
+    allBooks: async (root, args) => {
+      if (args.genre && args.author) {
+        const authorExists = await Author.findOne({ name: args.author })
+        if (!authorExists) {
+          return []
+        }
+        let filteredBooks = await Book.find({ author: authorExists._id })
+        return filteredBooks.filter(book => book.genres.includes(args.genre))
+      } else if (args.author) {
+        const authorExists = await Author.findOne({ name: args.author })
+        if (!authorExists) {
+          return []
+        }
+        let filteredBooks = await Book.find({ author: authorExists._id })
+        return filteredBooks
+      } else if (args.genre) {
+        let books = await Book.find({})
+        return books.filter(book => book.genres.includes(args.genre))
+      }
+
+      let all = await Book.find({})
+      return all
+    },
+    allAuthors: async () => Author.find({}),
+  },
+
+  Book: {
+    author: async root => {
+      return Author.findById(root.author)
+    },
+  },
+  Author: {
+    bookCount: async root => {
+      let books = await Book.countDocuments({ author: root._id })
+      return books
+    },
   },
 
   Mutation: {
     addBook: async (root, args) => {
-      let a = await Author.findOne({name: args.author})
+      let a = await Author.findOne({ name: args.author })
       if (!a) {
         a = new Author({ name: args.author })
-        await a.save()  
+        await a.save()
       }
 
       const book = new Book({
         title: args.title,
         author: a._id,
         published: args.published,
-        genres: args.genres
+        genres: args.genres,
       })
 
-      return book.save()
+      await book.save()
+      return book
     },
-    editAuthor: () => {}
-  }
+    editAuthor: async (root, args) => {
+      let a = await Author.findOne({ name: args.name })
+      if (!a) {
+        throw new GraphQLError('Author to update not found.', {
+          invalidArgs: args.name,
+        })
+      }
+
+      a.born = args.setBornTo
+      await a.save()
+      return a
+    },
+  },
 }
 
 const server = new ApolloServer({
